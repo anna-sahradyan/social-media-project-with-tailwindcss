f524219cb03ef7fa07b8386979970c012f470533
anna-sahradyan <a.sahradyan72@gmail.com>
2023-01-29 21:14:30 +0400

    added component and page directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Layout from "./components/Layout";
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from "react-toastify";
 
 const App = () => {
     return (
-        <div>
-          
-        </div>
+        <>
+          <Layout className={"flex w-full "}>
+              <ToastContainer/>
+
+          </Layout>
+        </>
     );
 };
 
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Link, NavLink} from "react-router-dom";
+import {toast} from "react-toastify";
+const Nav = () => {
+    const activeStyles = {
+        color: "white"
+    }
+
+    const logoutHandler = () => {
+        // dispatch(logout())
+        // window.localStorage.removeItem('token')
+        toast(`You have left from  system`);
+    }
+    const isAuth = false;
+    return (
+        <>
+            <div className={"flex py-4 justify-between items-center bg-blue-800 static"}>
+                <span
+                    className={"flex justify-center items-center w-40 h-8 bg-white text-sx text-black rounded-sm ml-8 font-bold font-sans"}>
+AnnaSocial
+                </span>
+                {isAuth &&
+                    <div className={" absolute  left-40"}>
+                        <ul className={"flex gap-40 "}>
+                            <li><NavLink to={"/"}
+                                         className={"text-sx text-black hover:text-white font-bold"}
+                                         style={({isActive}) => isActive ? activeStyles : undefined}>Home</NavLink></li>
+                            <li><NavLink to={"/post"} className={"text-sx text-black hover:text-white font-bold"}
+                                         style={({isActive}) => isActive ? activeStyles : undefined}>My
+                                Posts</NavLink></li>
+                            <li><NavLink to={"/new"} className={"text-sx text-black hover:text-white font-bold"}
+                                         style={({isActive}) => isActive ? activeStyles : undefined}>Add
+                                Post</NavLink></li>
+                        </ul>
+                    </div>
+                }
+
+                <div
+                    className={"flex justify-center items-center bg-white text-sx text-white rounded-sm px-4 absolute right-5"}>
+                    {isAuth ?
+                        (
+                            <button onClick={logoutHandler} className={"text-black font-bold items-center "}>sign
+                                out</button>) : (
+                            <Link to={'/login'} className={"text-black font-bold items-center "}>Sign in </Link>
+                        )}
+                </div>
+
+
+            </div>
+        </>
+
+)
+    ;
+};
+
+export default Nav;
